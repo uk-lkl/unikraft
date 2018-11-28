1d514b14a3b46a23571893b3cde359fb59a0ab6e
Akira Moroo <retrage01@gmail.com>
2018-11-28 15:30:39 +0900

    Revert thread.h

--- include/uk/arch/thread.h
+++ include/uk/arch/thread.h
@@ -36,14 +36,14 @@
 #ifndef __UKARCH_THREAD_H__
 #define __UKARCH_THREAD_H__
 
-#define <ucontext.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 struct ukplat_thread_ctx {
-  ucontext_t context;
+	/* keep in that order */
+	unsigned long sp;  /* Stack pointer */
+	unsigned long ip;  /* Instruction pointer */
 };
 
 #ifdef __cplusplus
